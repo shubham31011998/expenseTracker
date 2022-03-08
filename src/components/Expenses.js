691fb90e9b1bfac9135fac2366596b0c977e7163
shubham31011998 <shubhamyaduvanshi03@gmail.com>
2022-03-08 19:29:51 +0530

    tracker

---
+++ src/components/Expenses.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css'
+
+const Expenses = ({expenses}) => {
+    console.log('props', expenses)
+    // const items = 
+  return (
+    <div className='expenses'>
+        {expenses.map((expense)=>(
+           <ExpenseItem
+           title={expense.title}
+           amount={expense.amount}
+           date={expense.date}
+         /> 
+        ))}
+        {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      /> */}
+    </div>
+  );
+};
+
+export default Expenses;
